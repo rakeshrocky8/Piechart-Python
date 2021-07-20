6327103f01357861761b8caaa490ab112d09edf6
rakeshrocky8 <rakesh@gmail.com>
2021-07-20 19:51:15 +0530

    file adding

---
+++ Swapping/Swapping-Program.py
@@ -0,0 +1,12 @@
+print('Swapping Program\n\n')
+print('Enter any two number one by one')
+a=input()
+b=input()
+print('Before swapping a=',a)
+print('Before swapping b=',b)
+print('*************************************')
+temp = a
+a=b
+b=temp
+print('after swapping a=',a)
+print('after swapping b=',b)
